de921eef3830896794f873902128df5cde055904
swdavey <55132360+swdavey@users.noreply.github.com>
2022-07-07 14:05:08 +0100

    Update createdb.sql

--- scripts/createdb.sql
+++ scripts/createdb.sql
@@ -2,10 +2,10 @@ DROP DATABASE IF EXISTS db1;
 CREATE DATABASE db1;
 USE db1;
 CREATE TABLE t1 (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(40) NOT NULL
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(40) NOT NULL
 );
 INSERT INTO t1 (name) VALUES ('Fred'),('Barney'),('Wilma'),('Betty');
-SELECT * FROM t1;
-CREATE USER appuser@'%' IDENTIFIED BY 'Welcome1!';
-GRANT select,insert,update,delete ON db1.* TO appuser@'%';
+DROP USER IF EXISTS 'appuser'@'%';
+CREATE USER 'appuser'@'%' IDENTIFIED BY 'Welcome1!';
+GRANT Select, Insert, Update, Delete ON db1.* TO 'appuser'@'%';
